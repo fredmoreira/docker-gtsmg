6a82b358fcfae538ec8824c67a35fd7af9c76702
fredericomoreira <frederico.moreira@minutrade.com>
2015-11-13 16:33:09 -0200

    Versão 2.0 com logs

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-        res.send('API DEMONSTRAÇÃO DOCKER-UAITEST VERSÃO 1.0.0');
-    });
+	res.send('API DEMONSTRAÇÃO DOCKER-UAITEST VERSÃO 1.0.0');
+	console.log('GET - 200 - ' + new Date());
+});
 
 // Inicia o servidor na porta 5000
 app.set('port', process.env.PORT || 5000);
